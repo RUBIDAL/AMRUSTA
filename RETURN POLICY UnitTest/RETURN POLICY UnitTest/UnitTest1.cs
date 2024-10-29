05a4ae3e62c04dc19b4ee3ad3d31b0a79f08d626
RUBIDAL <louwrubida@gmail.com>
2024-10-29 13:35:47 +0200

    Add files via upload

---
+++ RETURN POLICY UnitTest/RETURN POLICY UnitTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ReturnPolicy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace RETURN_POLICY_UnitTest
+{
+    [TestClass]
+    public class ReturnPolicyTest
+    {
+        [TestMethod]
+        public void VerifyReturnPolicy()
+        {
+        //ARRANGE
+            var ReturnPolicyClass1 = new ReturnPolicyClass1() ;
+
+    //ACT
+        var returnWindow = ReturnPolicyClass1.GetReturnWindow();
+        var refundProcess = ReturnPolicyClass1.GetRefundProcess();
+
+    //ASSERT
+        Assert.AreEqual("30 days", returnWindow);
+        Assert.AreEqual("Full refund", refundProcess);
+        }
+    }
+}
